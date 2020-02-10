e0c83ad3452fafdede004e9d08bf7bb0403b2976
Rodger Jordas <rmjordas@gmail.com>
2020-02-11 07:17:50 +0800

    Rebuild site Tue, Feb 11, 2020  7:17:49 AM

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-44e42913c3063fa0e485.js"
+    "url": "webpack-runtime-9da2dd4153a13b4dfedd.js"
   },
   {
     "url": "styles.7d1922b93b683dc5df66.css"
@@ -36,17 +36,21 @@ self.__precacheManifest = [
     "url": "styles-7792292a17b3ed9e42be.js"
   },
   {
-    "url": "commons-fcb9d16c204e475961ef.js"
+    "url": "commons-dc57a13ac20665aa360b.js"
   },
   {
-    "url": "app-7a1f4e53725eff394d9d.js"
+    "url": "app-11515d5605b467057f37.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-ceef87afc058962c4db9.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "03dcdaa780d356a5635ae27339cbf2f5"
+    "revision": "0648728d815dc867896824fa7f0dd975"
+  },
+  {
+    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
+    "revision": "c355c8040c47a63bfb3360e4b7cb6553"
   },
   {
     "url": "manifest.webmanifest",
@@ -143,7 +147,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-7a1f4e53725eff394d9d.js`))) {
+  if (!resources || !(await caches.match(`/app-11515d5605b467057f37.js`))) {
     return await fetch(event.request)
   }
 
